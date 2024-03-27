799b6fe235352db5f643879535577574abd27c6e
Clinton James <clinton@jidn.com>
2024-03-27 15:53:41 -0500

    Add the edit subcommand

---
+++ src/lib.rs
@@ -0,0 +1,201 @@
+use std::env;
+use std::fs;
+use std::io::{Error, ErrorKind};
+use std::path::PathBuf;
+
+fn path_validate(path: &std::path::PathBuf, prefix: &str) -> Result<PathBuf, Error> {
+    let file_path = path.to_string_lossy().to_owned();
+    if path.exists() {
+        if path.is_file() {
+            // Return the path if it exists and is a file
+            Ok(path.clone())
+        } else {
+            Err(Error::new(
+                ErrorKind::InvalidInput,
+                format!("{}not a file: {}", prefix, file_path),
+            ))
+        }
+    } else {
+        Err(Error::new(
+            ErrorKind::NotFound,
+            format!("{}does not exist: {}", prefix, file_path),
+        ))
+    }
+}
+
+pub fn get_tsv_path(tsv: Option<std::path::PathBuf>) -> Result<PathBuf, Error> {
+    match tsv {
+        Some(path) => path_validate(&path, "--tsv "),
+        _ => {
+            let idid_tsv = "ididTSV";
+            // Existing "ididTSV" environment variable contains absolute path
+            if let Ok(value) = env::var(idid_tsv) {
+                let prefix = format!("${} ", idid_tsv);
+                return path_validate(&PathBuf::from(value), &prefix);
+            }
+            let env_xdg = "XDG_DATA_HOME";
+            match env::var(env_xdg) {
+                Err(e) => Err(Error::new(
+                    ErrorKind::NotFound,
+                    format!("${} does not exist: {}", env_xdg, e),
+                )),
+                Ok(value) => {
+                    let prefix = format!("${} ", env_xdg);
+                    let mut path = PathBuf::from(value);
+                    path.push("idid");
+                    path.push("idid.tsv");
+
+                    // Check for file $XDG_DATA_HOME/idid/idid.tsv
+                    match path_validate(&path, &prefix) {
+                        Ok(path) => Ok(path),
+                        Err(_e) => {
+                            // Create the file if possible
+                            match fs::File::create(&path) {
+                                Ok(_fc) => Ok(path),
+                                Err(e) => Err(e),
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // let env_var = "ididTSV";
+    // // Existing "ididTSV" environment variable contains absolute path
+    // if let Ok(value) = env::var(env_var) {
+    //     let path = PathBuf::from(&value);
+    //
+    //     // Check if the path exists and is a file
+    //     if path.exists() && path.is_file() {
+    //         return Ok(path);
+    //     } else {
+    //         return Err(format!(
+    //             "environment variable {env_var} of '{value}' is not a valid file path."
+    //         ));
+    //     }
+    // }
+    // let env_xdg = "XDG_DATA_HOME";
+    // // Check for file $XDG_DATA_HOME/idid/idid.tsv
+    // if let Ok(value) = env::var(env_xdg) {
+    //     let mut path = PathBuf::from(value);
+    //     path.push("idid");
+    //     path.push("idid.tsv");
+    //
+    //     // Check if the path exists and is a file
+    //     if path.exists() && path.is_file() {
+    //         return Ok(path);
+    //     } else {
+    //         // Create the file if possible
+    //         match fs::File::create(&path) {
+    //             Ok(_fc) => return Ok(path),
+    //             Err(e) => return Err(format!("Problem creating file: {:?}", e)),
+    //         };
+    //     }
+    // }
+    //
+    // // If neither variable exists, return an error
+    // Err(format!(
+    //     "Neither {env_var} nor {env_xdg} environment variables found."
+    // ))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::File;
+    use std::io::Error;
+    use tempfile::Builder;
+
+    #[test]
+    fn test_get_tsv_path_optional() {}
+
+    #[test]
+    fn test_get_tsv_path_ididtsv() -> Result<(), Error> {
+        let env_vars = ["ididTSV", "XDG_DATA_HOME"];
+        // Save the current values of the environment variables
+        let saved_values: Vec<Option<String>> = env_vars
+            .iter()
+            .map(|&var| save_environment_variable(var))
+            .collect();
+
+        // Create a tmp file
+        let temp_file = Builder::new().suffix(".txt").tempfile()?;
+        let file_path = temp_file.path().to_owned();
+
+        // Set the environment variable for testing purposes
+        env::set_var(env_vars[0], &file_path);
+        env::remove_var(env_vars[1]);
+
+        let result = get_tsv_path(None::<PathBuf>);
+
+        // Restore the original values of the environment variables or delete
+        // them if they didn't exist before
+        for (var, saved_value) in env_vars.iter().zip(saved_values) {
+            restore_environment_variable(var, saved_value);
+        }
+
+        // Remove the file
+        drop(temp_file);
+        let _ = std::fs::remove_file(&file_path);
+
+        assert!(result.is_ok());
+        Ok(())
+    }
+
+    #[test]
+    fn test_get_tsv_path_xdg() -> Result<(), Error> {
+        let env_vars = ["ididTSV", "XDG_DATA_HOME"];
+        // Save the current values of the environment variables
+        let saved_values: Vec<Option<String>> = env_vars
+            .iter()
+            .map(|&var| save_environment_variable(var))
+            .collect();
+
+        // Get the temporary directory path
+        let temp_dir = env::temp_dir();
+
+        // Create the "idid" directory inside the temporary directory
+        let idid_dir = temp_dir.join("idid");
+        fs::create_dir_all(&idid_dir)?;
+
+        // Create an empty file "idid.txt" inside the "idid" directory
+        let file_path = idid_dir.join("idid.tsv");
+        let temp_file = File::create(&file_path)?;
+        drop(temp_file);
+
+        // Set the environment variable for testing purposes
+        env::set_var(env_vars[1], &temp_dir);
+        env::remove_var(env_vars[0]);
+
+        let result = get_tsv_path(None::<PathBuf>);
+
+        // Restore the original values of the environment variables or delete
+        // them if they didn't exist before
+        for (var, saved_value) in env_vars.iter().zip(saved_values) {
+            restore_environment_variable(var, saved_value);
+        }
+
+        // Remove the file
+        std::fs::remove_file(&file_path)?;
+
+        assert!(result.is_ok());
+        Ok(())
+    }
+
+    // Function to save the current value of an environment variable
+    fn save_environment_variable(name: &str) -> Option<String> {
+        match env::var(name) {
+            Ok(value) => Some(value),
+            Err(_) => None,
+        }
+    }
+    // Function to restore the original value of an environment variable
+    // or delete if it didn't exist before
+    fn restore_environment_variable(name: &str, saved_value: Option<String>) {
+        match saved_value {
+            Some(value) => env::set_var(name, &value),
+            None => env::remove_var(name),
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,149 @@
+use clap::{Parser, Subcommand};
+use std::env;
+use std::path::PathBuf;
+use std::process::Command;
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Add accomplishment.
+    #[command(arg_required_else_help = true)]
+    Add {
+        /// Either WHEN minutes ago or at HH:MM.
+        #[arg(short = 't', value_name = "WHEN")]
+        offset: Option<String>,
+
+        /// Text to record
+        #[arg(required = true)]
+        text: Vec<String>,
+    },
+    /// Edit TSV file using $EDITOR.
+    Edit,
+
+    /// Start recording time.
+    Start {
+        /// Either WHEN minutes ago or HH:MM.
+        #[arg(short = 't', value_name = "WHEN")]
+        offset: Option<String>,
+    },
+
+    // The following need selectors and filters
+    // Date can be any of:
+    //     Number of days before today; zero is today and one is yesterday.
+    //     Literal text of 'today' or 'yesterday'.
+    //     ISO 8601 date format 'YYYY-MM-DD' or 'MM-DD' the last 364ish days.
+    //         Dashes are optional.
+    //     The locale abbreviated, last day-of-the-week. A suffix adds weeks.
+    //         'mon' and 'mon1' is last Monday and 'mon2' is two Mondays ago.
+    // --date DATE[,DATE]...    multiple allowed
+    // --range DATE DATE        multiple allowed
+    //
+    // Filters
+    // -x --exclude string
+    // -i --include string   or -f --find string
+    /// Lines for processing.
+    Lines,
+
+    /// Show accomplishments.
+    Show,
+
+    /// Summarize accomplishments.
+    Summary,
+}
+
+#[derive(Parser)]
+#[command(version, long_about = None)] // read from Cargo.toml
+#[command(about = "Add, edit, and show accomplishments.")]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+
+    /// Use a custom TSV file instead of $ididTSV or $XDG_DATA_HOME
+    #[arg(long, value_name = "FILE")]
+    tsv: Option<PathBuf>,
+}
+
+fn get_string(option_str: Option<String>) -> String {
+    option_str.unwrap_or(String::from("NONE"))
+}
+
+fn main() {
+    let cli = Cli::parse();
+    let tsv: String = idid::get_tsv_path(cli.tsv)
+        .unwrap()
+        .to_string_lossy()
+        .to_string();
+    match cli.command {
+        Some(Commands::Add { offset, text }) => {
+            println!(
+                "Add offset={}, tsv={}, text='{}'",
+                get_string(offset),
+                tsv,
+                text.join(" "),
+            );
+        }
+        Some(Commands::Edit {}) => {
+            // Get the value of the EDITOR environment variable
+            let editor = match env::var("EDITOR") {
+                Ok(val) => val,
+                Err(_) => {
+                    eprintln!("EDITOR environment variable is not set");
+                    std::process::exit(1);
+                }
+            };
+
+            let mut command = Command::new(&editor);
+            command.arg(tsv);
+
+            // Check if the editor is a vi variant
+            if editor.ends_with("vi") || editor.ends_with("vim") {
+                // Open at the end of the file
+                command.arg("+$");
+            }
+
+            let status = command.status().expect("Failed to start editor process");
+
+            if !status.success() {
+                eprintln!("Editor process failed with error code: {:?}", status.code());
+            }
+            // match std::env::var("EDITOR") {
+            //     Ok(value) => {
+            //         println!("editor= '{}'", value);
+            //         // let mut args = vec![value.clone()];
+            //         // // path could have spaces, /Users/Clinton James/AppData/
+            //         // args.push(format!("\"{tsv}\""));
+            //         // if value.ends_with("vi") || value.ends_with("vim") {
+            //         //     // Open at the end of the file.
+            //         //     args.push(String::from("+$"));
+            //         // }
+            //         // println!("cmdline= '{}'", args.join(" "));
+            //
+            //         let mut cmd = Command::new(&value).arg(&tsv);
+            //         if value.ends_with("vi") || value.ends_with("vim") {
+            //             cmd.arg("+$");
+            //         }
+            //
+            //         cmd.status().expect("failed to execute process");
+            //     }
+            //     Err(e) => {
+            //         eprintln!("$EDITOR not found: {e}");
+            //         std::process::exit(1);
+            //     }
+            // }
+        }
+        Some(Commands::Start { offset }) => {
+            println!("Start t={}, tsv={}", get_string(offset), tsv);
+        }
+        Some(Commands::Lines {}) => {
+            println!("Lines tsv={}", tsv);
+        }
+        Some(Commands::Show {}) => {
+            println!("Show tsv={}", tsv);
+        }
+        Some(Commands::Summary {}) => {
+            println!("Summary tsv={}", tsv);
+        }
+        None => {
+            println!("Show current timing tsv={}", tsv);
+        }
+    }
+}
